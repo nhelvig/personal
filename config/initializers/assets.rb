28459928f281a179c2c8bcd9c5ece36be598ae79
nhelvig <nhelvig@thoughtworks.com>
2016-03-12 09:07:43 -0700

    Added travel pictures and made picture page.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( hover.css )
 Rails.application.config.assets.precompile += %w( picture-slideshow.js )
 Rails.application.config.assets.precompile += %w( travel-map.js )
+Rails.application.config.assets.precompile += %w( pics.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
