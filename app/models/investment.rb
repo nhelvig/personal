75ee0a40e87e682f7ec034822cb6c320a1bbc6c4
nhelvig <nhelvig@thoughtworks.com>
2016-04-13 20:44:39 -0400

    Updating investments

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -10,7 +10,7 @@ class Investment < ActiveRecord::Base
       total_value = transaction.price * transaction.quantity
       @@total_invested += total_value
     else
-      # print "Cannot sell something that is not owned"
+      raise 'Cannot sell something that is not owned'
     end
     investment = Investment.new
     investment.symbol = transaction.symbol
@@ -24,26 +24,31 @@ class Investment < ActiveRecord::Base
   def addTransaction(transaction)
     investment_value = transaction.quantity * transaction.price
     if transaction.action == 'buy'
-      total_quantity = quantity + transaction.quantity
-      new_avg_cost = (avg_cost * (quantity / total_quantity) + transaction.price * (transaction.quantity / total_quantity)).round(2)
-      update_attribute(:avg_cost, new_avg_cost)
-      update_attribute(:quantity, total_quantity)
-      update_attribute(:total_value, investment_value + total_value)
-      updateTotalInvested(investment_value)
-      updateAvailableCash(investment_value)
-      print "total_invested: "
-      print @@total_invested
+      handleBuy(investment_value, transaction)
       save
     elsif transaction.action == 'sell'
-      total_quantity = quantity - transaction.quantity
-      update_attribute(:quantity, total_quantity)
-      update_attribute(:total_value, total_value - investment_value)
-      @@available_cash += investment_value
+      handleSell(investment_value, transaction)
     else
-      print "This is not right"
-      # throw new error ("action not a buy or sell")
+      raise 'The transaction is somehow not a buy or a sell'
     end
 
+  end
+
+  def handleBuy(investment_value, transaction)
+    total_quantity = quantity + transaction.quantity
+    new_avg_cost = (avg_cost * (quantity / total_quantity) + transaction.price * (transaction.quantity / total_quantity)).round(2)
+    update_attribute(:avg_cost, new_avg_cost)
+    update_attribute(:quantity, total_quantity)
+    update_attribute(:total_value, investment_value + total_value)
+    updateTotalInvested(investment_value)
+    updateAvailableCash(investment_value)
+  end
+
+  def handleSell(investment_value, transaction)
+    total_quantity = quantity - transaction.quantity
+    update_attribute(:quantity, total_quantity)
+    update_attribute(:total_value, total_value - investment_value)
+    @@available_cash += investment_value
   end
 
   def updateAvailableCash(investment_value)
@@ -54,22 +59,9 @@ class Investment < ActiveRecord::Base
   end
 
   def updateTotalInvested(investment_value)
-    print "updateTotalInvested \n"
-    print "investment_value: \n"
-    print investment_value
-    print "\n"
-    print "available_cash: \n"
-    print @@available_cash
-    print "\n"
-    print "total_invested: \n"
-    print @@total_invested
-    print "--------------\n"
     if investment_value - @@available_cash > 0
       @@total_invested = @@total_invested + investment_value - @@available_cash
     end
-    print "total_invested: \n"
-    print @@total_invested
-    print "\n"
   end
 
   def get_stock_price
@@ -102,12 +94,9 @@ class Investment < ActiveRecord::Base
 
   def self.totalValue
     sum = 0
-
     for investment in Investment.all
        sum += investment.value
     end
-    print "SUM: \n"
-    print sum
     return sum + @@available_cash
   end
 
