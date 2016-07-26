ddf59ec14d27f431491e68624b6d07c3e801c824
nhelvig <nhelvig@thoughtworks.com>
2016-07-25 22:29:30 -0400

    Added tests

--- test/models/investment_test.rb
+++ test/models/investment_test.rb
@@ -1,25 +1,74 @@
 require 'test_helper'
 
 class InvestmentTest < ActiveSupport::TestCase
-  transaction = Transaction.new({"symbol" => "GE", "quantity" => 2, "price" => 10.0, "action" => "buy"})
+
+  def setup
+    @firstTransaction = Transaction.new({"symbol" => "GE", "quantity" => 5, "price" => 10.0, "action" => "buy"})
+    @buyTransaction = Transaction.new({"symbol" => "GE", "quantity" => 5, "price" => 20.0, "action" => "buy"})
+    @sellTransaction = Transaction.new({"symbol" => "GE", "quantity" => 1, "price" => 20.0, "action" => "sell"})
+    @investment = Investment.build(@firstTransaction)
+  end
+  def teardown
+    Investment.destroy("GE")
+    Investment.setTotalInvested(0)
+  end
 
   test "A new investment should have a symbol, quantity, total value, current price, and average cost" do
-    investment = Investment.build(transaction)
-    assert_not_nil(investment.symbol, "No symbol when the investment was created")
-    assert_not_nil(investment.quantity, "No quantity when the investment was created")
-    assert_not_nil(investment.total_value, "No total value when the investment was created")
-    assert_not_nil(investment.current_price, "No current price when the investment was created")
-    assert_not_nil(investment.avg_cost, "No average cost when the investment was created")
+    assert_not_nil(@investment.symbol, "No symbol when the investment was created")
+    assert_not_nil(@investment.quantity, "No quantity when the investment was created")
+    assert_not_nil(@investment.total_value, "No total value when the investment was created")
+    assert_not_nil(@investment.current_price, "No current price when the investment was created")
+    assert_not_nil(@investment.avg_cost, "No average cost when the investment was created")
   end
 
-  test "An new investment should have the same information as the transaction it was started with" do
-    investment = Investment.build(transaction)
-
-    assert_equal(investment.symbol, transaction.symbol, "Symbols do not match")
-    assert_equal(investment.quantity, transaction.quantity, "Quantity does not match")
-    assert_equal(investment.total_value, transaction.price * transaction.quantity, "Value does not match price * quantity")
-    assert_equal(investment.current_price, transaction.price, "Current price does not match the transaction price")
-    assert_equal(investment.avg_cost, transaction.price, "Average cost does not match the transaction price")
+  test "A new investment should have the same information as the transaction it was started with" do
+    assert_equal(@investment.symbol, @firstTransaction.symbol, "Symbols do not match")
+    assert_equal(@investment.quantity, @firstTransaction.quantity, "Quantity does not match")
+    assert_equal(@investment.total_value, @firstTransaction.price * @firstTransaction.quantity, "Value does not match price * quantity")
+    assert_equal(@investment.current_price, @firstTransaction.price, "Current price does not match the transaction price")
+    assert_equal(@investment.avg_cost, @firstTransaction.price, "Average cost does not match the transaction price")
   end
-  
+
+  test "A single buy transaction should update the average cost" do
+    @investment.addTransaction(@buyTransaction)
+
+    assert_equal("15.0", @investment.avg_cost.to_s)
+  end
+
+  test "A single buy transaction should update the quantity" do
+    @investment.addTransaction(@buyTransaction)
+
+    assert_equal("10.0", @investment.quantity.to_s)
+  end
+
+  test "A single buy transaction should update the total value" do
+    @investment.addTransaction(@buyTransaction)
+
+    assert_equal("150.0", @investment.total_value.to_s)
+  end
+
+  test "A single buy transaction should update the total invested" do
+    @investment.addTransaction(@buyTransaction)
+
+    assert_equal("150.0", Investment.getTotalInvested.to_s)
+    end
+
+  test "A single sell transaction should update the quantity" do
+    @investment.addTransaction(@sellTransaction)
+
+    assert_equal("4.0", @investment.quantity.to_s)
+  end
+
+  test "A single sell transaction should update the total value" do
+    @investment.addTransaction(@sellTransaction)
+
+    assert_equal("30.0", @investment.total_value.to_s)
+  end
+
+  test "A single sell transaction should not change the total invested" do
+    @investment.addTransaction(@sellTransaction)
+
+    assert_equal("50.0", Investment.getTotalInvested.to_s)
+  end
+
 end
